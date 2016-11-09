7e45781bc8e0c766bed6d47f3da65371f76e593f
jeremy <jeraguilon@gmail.com>
2016-11-08 23:11:05 -0500

    Historical report operations implemented

---
+++ app/models/historicalreport.js
@@ -0,0 +1,10 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+module.exports = mongoose.model('HistoricalReport', new Schema({
+	'historicalReportType' : {type:Number, required: true},
+	'ppm' : {type:Number, required: true},
+	'location' : {type:String, required: true},
+}, {
+	'timestamps' : true
+}));
